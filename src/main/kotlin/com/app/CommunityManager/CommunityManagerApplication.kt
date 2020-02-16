1aa5b0630082cca0b5bd8c4bdfa461c41cfe58d6
SamusVD <samueldanso@outlook.fr>
2020-02-16 15:16:35 +0100

    Setting backend project

---
+++ src/main/kotlin/com/app/CommunityManager/CommunityManagerApplication.kt
@@ -0,0 +1,11 @@
+package com.app.CommunityManager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CommunityManagerApplication
+
+fun main(args: Array<String>) {
+	runApplication<CommunityManagerApplication>(*args)
+}
